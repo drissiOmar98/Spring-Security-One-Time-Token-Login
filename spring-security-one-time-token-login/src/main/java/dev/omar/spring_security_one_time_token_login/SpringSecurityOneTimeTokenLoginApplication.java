9cca7d0b7cd61b03451f45890b7eb254e8b0b896
DrissiOmar <omar.drissi@esprit.tn>
2025-10-21 14:09:57 +0100

    Initialize Spring Boot project

---
+++ spring-security-one-time-token-login/src/main/java/dev/omar/spring_security_one_time_token_login/SpringSecurityOneTimeTokenLoginApplication.java
@@ -0,0 +1,13 @@
+package dev.omar.spring_security_one_time_token_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOneTimeTokenLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOneTimeTokenLoginApplication.class, args);
+	}
+
+}
---
+++ spring-security-one-time-token-login/src/test/java/dev/omar/spring_security_one_time_token_login/SpringSecurityOneTimeTokenLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.omar.spring_security_one_time_token_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOneTimeTokenLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
